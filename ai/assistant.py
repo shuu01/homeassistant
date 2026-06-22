896a866f0a388c699f1050818ae88eece0a8ff90
shuu01 <shuu01@gmail.com>
2026-06-22 11:47:45 +0700

    assistant: add logger

--- ai/assistant.py
+++ ai/assistant.py
@@ -21,6 +21,11 @@ from openwakeword.model import Model
 
 import logging
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 logger = logging.getLogger(__name__)
 
 MIC_RATE = 48000
